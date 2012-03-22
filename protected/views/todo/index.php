a4d37a20cdf4566f79ed7787f68e2edb00795594
KAT <hesperis2@gmail.com>
2012-03-22 22:02:21 +0100

    Refactored views, removed default css files, added new css: style.css

---
+++ protected/views/todo/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->baseUrl; ?>/css/style.css">
+</head>
+
+<body></body>
+
+</html>
