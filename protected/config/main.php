25d43b51deac535d7d58de39c1d1ed049316c860
KAT <hesperis2@gmail.com>
2012-03-22 22:10:36 +0100

    Configured db settings

--- protected/config/main.php
+++ protected/config/main.php
@@ -49,18 +49,13 @@ return array(
 		),
 
 		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
-		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=todo',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '',
 			'charset' => 'utf8',
 		),
-		*/
+
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
             'errorAction'=>'todo/error',
