c907b67c09b937806929799f8284f12407723a3e
KAT <hesperis2@gmail.com>
2012-04-27 21:25:03 +0200

    Cleaner code for ToDos controller.

--- assets/app/controller/ToDos.js
+++ assets/app/controller/ToDos.js
@@ -5,6 +5,17 @@ Ext.define('ToDo.controller.ToDos', {
     models: ['ToDo'],
     views:  ['todo.List'],
 
+    refs: [{
+        ref: 'ToDo',
+        selector: '#toDoTextField'
+    }, {
+        ref: 'DueDate',
+        selector: '#dueDateField'
+    }, {
+        ref: 'Viewport',
+        selector: 'viewport'
+    }],
+
     init: function() {
         this.control({
             '#toDoTextField': { //FIXIT: how to join it with #dueDateField
@@ -14,10 +25,10 @@ Ext.define('ToDo.controller.ToDos', {
                 specialkey: this.onSpecialKey
             },
             'button[action=onAddClick]': {
-                click: this.onAddClick
+                click: this.create
             },
             'button[action=onDeleteClick]': {
-                click: this.onDeleteClick
+                click: this.delete
             },
             'actioncolumn': { //FIXIT: why actioncolumn[action=onRowDeleteClick] doesnt work?
                 click: this.onRowDeleteClick
@@ -27,41 +38,40 @@ Ext.define('ToDo.controller.ToDos', {
 
     onSpecialKey: function(field, e) {
         if (e.getKey() == e.ENTER) {
-            this.onAddClick();
+            this.create();
         }
     },
 
-    onAddClick: function()
-    {
-        var toDoTextField = Ext.getCmp('toDoTextField'),
-            dueDateField = Ext.getCmp('dueDateField'),
-            toDo = toDoTextField.getValue(),
-            dueDate = Ext.util.Format.date(dueDateField.getValue(), 'Y-m-d'),
-            model = new ToDo.model.ToDo({toDo: toDo, dueDate: dueDate}),
-            store = Ext.getStore('ToDos');
+    create: function() {
+        var model = this.getToDoModel(),
+            store = this.getToDosStore(),
+            toDoModel = new model({
+                toDo: this.getToDo().getValue(),
+                dueDate: this.getDueDate().getValue()
+            }),
+            errors = toDoModel.validate();
 
-        if (toDoTextField.isValid() && dueDateField.isValid())
-        {
-            store.insert(0, model);
-            store.totalCount++;
+        if (errors.isValid()) {
+            store.insert(0, toDoModel);
             store.sync();
 
-            toDoTextField.reset();
-            dueDateField.reset();
+            this.getToDo().reset();
+            this.getDueDate().reset();
+        }
+        else {
+            toDoModel.logErrors(errors);
         }
     },
 
-    onDeleteClick: function()
-    {
-        var store = Ext.getStore('ToDos'),
-            list = Ext.getCmp('toDoList'),
-            selections = list.getSelectionModel().getSelection();
+    delete: function() {
+        var store = this.getToDosStore(),
+            list = this.getViewport().getToDoList(),
+            records = list.getSelectionModel().getSelection();
 
-        if (selections.length > 0) {
+        if (records.length > 0) {
             Ext.MessageBox.confirm('Confirm', 'Are you sure?', function(btn) {
                 if (btn == 'yes'){
-                    store.remove(selections);
-                    store.totalCount -= selections.length;
+                    store.remove(records);
                     store.sync();
                     list.getSelectionModel().deselectAll();
                 }
@@ -69,9 +79,8 @@ Ext.define('ToDo.controller.ToDos', {
         }
     },
 
-    onRowDeleteClick: function(grid, html, rowIndex)
-    {
-        var store = Ext.getStore('ToDos');
+    onRowDeleteClick: function(grid, html, rowIndex) {
+        var store = this.getToDosStore();
 
         Ext.MessageBox.confirm('Confirm', 'Are you sure?', function(btn) {
             if (btn == 'yes') {
